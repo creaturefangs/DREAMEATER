711a0cec01d1a558b4b277600221a2f18758a322
creaturefangs <126419720+creaturefangs@users.noreply.github.com>
2024-06-24 19:11:25 -0400

    first commit

---
+++ Assets/Scripts/Player/PlayerMovement.cs
@@ -0,0 +1,36 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public float moveSpeed = 5f; // Speed of the player
+
+    public Rigidbody2D rb; // Reference to the player's Rigidbody2D component
+    public Animator animator; // Reference to the player's Animator component
+
+    private Vector2 movement; // Store the player's movement
+
+    // Update is called once per frame
+    void Update()
+    {
+        // Input
+        movement.x = Input.GetAxisRaw("Horizontal");
+        movement.y = Input.GetAxisRaw("Vertical");
+
+        // Update animation parameters
+        animator.SetFloat("Horizontal", movement.x);
+        animator.SetFloat("Vertical", movement.y);
+        animator.SetFloat("Speed", movement.sqrMagnitude);
+    }
+
+    // FixedUpdate is called at a fixed interval and is independent of frame rate
+    void FixedUpdate()
+    {
+        // Move the player
+        rb.MovePosition(rb.position + movement * moveSpeed * Time.fixedDeltaTime);
+    }
+}
+
---
+++ Assets/Scripts/UI/SceneChanger.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneChanger : MonoBehaviour
+{
+    public float changeTime;
+    public string SceneName;
+
+    // Update is called once per frame
+    void Update()
+    {
+        changeTime -= Time.deltaTime;
+        if (changeTime <= 0)
+        {
+            SceneManager.LoadScene(SceneName);
+        }
+    }
+}
